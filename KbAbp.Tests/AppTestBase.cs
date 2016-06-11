9c79993a2eca8068460bdf8efc77eae0eee3c3a2
Peng zhen <1050244110@qq.com>
2016-06-11 12:41:48 +0800

    #添加测试项目

---
+++ KbAbp.Tests/AppTestBase.cs
@@ -0,0 +1,27 @@
+﻿using Abp.Collections;
+using Abp.Modules;
+using Abp.TestBase;
+using Castle.MicroKernel.Registration;
+using Effort;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KbAbp.Tests
+{
+    public abstract class AppTestBase : AbpIntegratedTestBase
+    {
+        public AppTestBase()
+        {
+
+        }
+
+        public AppTestBase(bool isReal)
+        {
+
+        }
+    }
+}
