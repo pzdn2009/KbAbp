5321a49615132c51d198164657c1d1dde3b974d1
Peng zhen <1050244110@qq.com>
2016-06-16 17:06:06 +0800

    #jTable paging parameter

---
+++ KbAbp.Application/JTablePagingGridInput.cs
@@ -0,0 +1,16 @@
+﻿using Abp.Application.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KbAbp
+{
+    public class JTablePagingGridInput : IInputDto
+    {
+        public int JtStartIndex { get; set; }
+        public int JtPageSize { get; set; }
+        public string JtSorting { get; set; }
+    }
+}
