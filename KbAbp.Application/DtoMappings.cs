f39e2ed9ddff5ce4589bc0a606366810d5f20653
Peng zhen <1050244110@qq.com>
2016-06-16 14:10:14 +0800

    #include DtoMappings

---
+++ KbAbp.Application/DtoMappings.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using KbAbp.Kbs;
+using KbAbp.Kbs.Dtos;
+using KbAbp.Projects;
+using KbAbp.Projects.Dtos;
+using KbAbp.Tasks;
+using KbAbp.Tasks.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KbAbp
+{
+    internal static class DtoMappings
+    {
+        public static void Map()
+        {
+            Mapper.CreateMap<Task, TaskDto>().ForMember(dest => dest.ProjectName, option => option.MapFrom(
+                 src => src.Project != null ? src.Project.Name : string.Empty));
+
+            Mapper.CreateMap<Project, ProjectDto>();
+
+            Mapper.CreateMap<KbQueue, KbQueueDto>();
+
+            Mapper.CreateMap<KbCategory, KbCategoryDto>();
+        }
+    }
+}
